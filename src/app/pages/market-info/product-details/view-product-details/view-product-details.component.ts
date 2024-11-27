ea1608a8c7ab54ea33aad0ff2c363741b7539612
Souradeep Sarkar <88938453+SouradeepSarkar77@users.noreply.github.com>
2024-11-27 10:36:00 +0530

    view product details also working

--- src/app/pages/market-info/product-details/view-product-details/view-product-details.component.ts
+++ src/app/pages/market-info/product-details/view-product-details/view-product-details.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Inject, OnInit } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
-// @import '~bootstrap/dist/css/bootstrap.min.css';
 
 interface ProductImage {
   id: number;
@@ -18,45 +17,20 @@ export class ViewProductDetailsComponent implements OnInit {
   imageUrl: any;
   res: any;
   productID: any;
-  images: any[] = [];
+  images: ProductImage[] = []; // Correctly typed array
 
-  constructor(private dialog:MatDialog,
+  constructor(private dialog: MatDialog,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private http: HttpClient,) {
-      this.productID= data.element.id
-     }
+    private http: HttpClient) {
+      this.productID = data.element.id;
+  }
 
   ngOnInit(): void {
     this.getProductImages();
   }
-  // getProductImages(){
-  //   try {
-  //     var httpBody: Object={
-  //       "productId": this.productID
-  //     }
-  //     this.http.get(`https://api-dev.themafic.co.in/api/products/images?id=${this.productID}`).subscribe(data => {
-  //       console.log(data);
-  //       this.res = data;
-  //       console.log(this.res)
-  //       if (this.res.status == 200) {
-  //         this.images = this.res.data.images;
-  //         console.log(this.images)
-  //       }
-
-  //     }, error => { },
-  //       () => {});
-
-  //   }
-  //   catch (error) {
-  //     console.error("not able to get response from getProductCategory API")
-  //   }
-  // }
 
   getProductImages() {
     try {
-      var httpBody: Object = {
-        "productId": this.productID
-      };
       this.http.get(`https://api-dev.themafic.co.in/api/products/images?id=${this.productID}`).subscribe(
         (data: any) => {
           console.log(data);
@@ -64,15 +38,12 @@ export class ViewProductDetailsComponent implements OnInit {
           console.log(this.res);
           if (this.res.status === 200) {
             // Map the images from the data array
-            this.images = this.res.data.map((item: ProductImage) => item.images);
+            this.images = this.res.data.map((item: ProductImage) => ({ id: item.id, images: item.images }));
             console.log(this.images);
           }
         },
         error => {
           console.error("Error occurred while fetching product images:", error);
-        },
-        () => {
-          // Optional complete handler
         }
       );
     } catch (error) {
@@ -80,7 +51,7 @@ export class ViewProductDetailsComponent implements OnInit {
     }
   }
 
-  closepopup(){
+  closepopup() {
     this.dialog.closeAll();
   }
 }
